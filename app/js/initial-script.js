fe5d8f16ba7a9abc244034bdc04fb2a1f2797d60
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-06-25 12:52:18 -0500

    Implementa segunda galería (Bootstrap, jQuery, JSON y Mustache) Para evitar incompatibilidades reescribe nombre de la clase "col" por "column" los tags HTML y clases CSS para la primera galería

--- app/js/initial-script.js
+++ app/js/initial-script.js
@@ -5,8 +5,17 @@ global.jQuery = require( 'jquery' ); /* Declaramos de forma global las dependenc
 bootstrap = require( 'bootstrap' );
 Mustache = require( 'mustache' );
 
-jQuery( document ) .ready( function() {
-  saluda( ' usando el paquete "concat" en Sass e integrando \n"jQuery", "Bootstrap" y "Mustache" \nal proyecto usando "gulp-browserify"' );
+jQuery( document ) .ready( function( $ ) {
+  /* Obtenemos los datos del archivo JSON haciendo un "Request" a través de AJAX y lo asignamos a una variable */
+  var xhr_data = $ .getJSON( 'data.json', function() {
+
+  }). done( function( data ){   /* Usamos el método "done" para garantizar que solo cuando finalice el "Request"
+                                   va a ejecutar una acción (a través de una función anónima) */
+    console .log( data );
+    var template = $( '#template' ) .html();                 /* Busca ID con el nombre de "template" */
+    var showTemplate = Mustache.render( template, data );    /* Delegamos a "Mustache" el formateo de los datos */
+    $( '#galeria-londres' ) .html( showTemplate );           /* Desplegamos en la vista donde el TAG tenga un ID con el nombre "galeria-londres"  */
+  });
 });
 
 /* Funciones */
